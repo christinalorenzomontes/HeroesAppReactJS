80f01659b83965c9a664d882150f0b90b169a107
Christina Lorenzo <christinalorenzo@gmail.com>
2024-03-12 22:07:11 +0100

    heroScreen

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,8 +1,26 @@
+import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const hero = getHeroById( id );
+
+  if ( !hero ) {
+    return <Navigate to="/marvel" />
+  }
+  
   return (
-    <>
-      <h1>HeroPage</h1>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img 
+          src="" 
+          alt="" 
+          className="img-thumbnail"  
+        />
+      </div>
+    </div>
   )
 }
