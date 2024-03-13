49cbeae903297c3135c3ed112c8e7a3be9909e04
Christina Lorenzo <christinalorenzo@gmail.com>
2024-03-13 13:24:45 +0100

    add design searchPage

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,8 +1,35 @@
+import { HeroCard } from "../components";
 
 export const SearchPage = () => {
   return (
     <>
-      <h1>SearchPage</h1>
+      <h1>Search</h1>
+      <hr />
+
+    <div className="row">
+      <div className="col-5">
+        <h2>Searching</h2>
+        <hr />
+        <form>
+          <input type="text" placeholder="Search a hero" className="form-control" name="searchText" autoComplete="off" />
+          <button className="btn btn-outline-primary mt-1">
+            Seacrh
+          </button>
+        </form>
+      </div>
+
+      <div className="col-7">
+        <h2>Results:</h2>
+        <hr />
+        <div className="alert alert-primary">
+          Search a hero
+        </div>
+        <div className="alert alert-danger">
+          No hero with <b>ABC</b>
+        </div>
+        <HeroCard />
+      </div>
+    </div>
     </>
   )
 }
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -16,7 +16,7 @@ export const Navbar = () => {
         className="navbar-brand" 
         to="/"
       >
-        Asociaciones
+        Associations
       </Link>
 
       <div className="navbar-collapse">
