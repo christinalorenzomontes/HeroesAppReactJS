962238832bd3f81ba22c1425a17cecf953cfafec
Christina Lorenzo <christinalorenzo@gmail.com>
2024-03-14 12:51:33 +0100

    logout

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,9 +4,9 @@ import { authReducer } from "./authReducer"
 
 import { types } from "../types/types"
 
-const initialState = {
-  logged: false,
-}
+// const initialState = {
+//   logged: false,
+// }
 
 const init = () => {
   const user = JSON.parse(localStorage.getItem('user') );
@@ -19,26 +19,32 @@ const init = () => {
 
 export const AuthProvider = ({ children }) => {
 
-  const [ authState, dispatch ] = useReducer( authReducer, initialState, init );
+  const [ authState, dispatch ] = useReducer( authReducer, {}, init );
 
   const login = ( name = '' ) => {
 
     const user = { id: 'ABC', name };
 
-    const action = {
-      type: types.login,
-      payload: user
-    }
+    const action = { type: types.login, payload: user }
 
     localStorage.setItem('user', JSON.stringify( user ) );
 
-    dispatch(action)
+    dispatch(action);
+  }
+
+  const logout = () => {
+    localStorage.removeItem('user');
+
+    const action = { types: types.logout };
+
+    dispatch(action);
   }
 
   return (
     <AuthContext.Provider value={{
       ...authState,
-      login: login
+      login: login,
+      logout: logout
     }}>
       { children }
     </AuthContext.Provider>
--- src/auth/context/authReducer.js
+++ src/auth/context/authReducer.js
@@ -3,6 +3,7 @@ import { types } from "../types/types";
 export const authReducer = ( state = {}, action ) => {
 
   switch ( action.type ) {
+    
     case types.login:
       return {
         ...state,
