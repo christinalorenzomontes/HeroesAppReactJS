7462e4269f3bcbc424a3a8ab303b89157a6520df
Christina Lorenzo <christinalorenzo@gmail.com>
2024-03-18 14:35:32 +0100

    test AppRouter

---
+++ test/router/AppRouter.test.jsx
@@ -0,0 +1,48 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { AuthContext } from '../../src/auth';
+import { AppRouter } from '../../src/router/AppRouter';
+
+describe('Pruebas en <AppRouter />', () => {
+    
+  test('debe de mostrar el login si no está autenticado', () => {
+
+    const contextValue = {
+      logged: false,
+    }
+
+    render(
+      <MemoryRouter initialEntries={['/marvel']}>
+        <AuthContext.Provider value={ contextValue }>
+          <AppRouter />
+        </AuthContext.Provider>
+      </MemoryRouter>
+    );
+
+    expect( screen.getAllByText('login').length ).toBe(2)
+      
+  });
+
+  test('Debe mostrar el componente de Marvel si está autenticado', () => {
+  
+    const contextValue = {
+      logged: true,
+      user: {
+          id: 'ABC',
+          name: 'Chris'
+      }
+    }
+
+    render(
+      <MemoryRouter initialEntries={['/login']}>
+          <AuthContext.Provider value={ contextValue }>
+              <AppRouter />
+          </AuthContext.Provider>
+      </MemoryRouter>
+    );
+
+    expect( screen.getAllByText('Marvel').length ).toBeGreaterThanOrEqual(1);
+
+  });
+
+});
